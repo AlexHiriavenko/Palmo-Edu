fa2192221f594fc5b43fdff5734da65df9345d5f
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2024-07-02 17:55:54 +0300

    update

--- JS/HW3/main.js
+++ JS/HW3/main.js
@@ -1,4 +1,12 @@
 import printEvenNumbers from "./tasks/Part1_Task1_printEvenNumbers.js";
+import printMultiplicationTable from "./tasks/Part1_Task2_printMultiplicationTable.js";
+import leastCommonMultiple from "./tasks/Part1_Taks2_leastCommonMultiple.js";
 
 // Part 1 Task 1
-// printEvenNumbers(0, 20);
+// printEvenNumbers();
+
+// Part 1 Task 2
+// printMultiplicationTable();
+
+// Part 1 Task 3
+leastCommonMultiple();
--- JS/HW3/tasks/Part1_Task1_printEvenNumbers.js
+++ JS/HW3/tasks/Part1_Task1_printEvenNumbers.js
@@ -1,4 +1,4 @@
-function printEvenNumbers(a, b) {
+function printEvenNumbers(a = 0, b = 20) {
   for (let i = a; i <= b; i += 1) {
     if (i % 2 === 0) {
       console.log(i);
