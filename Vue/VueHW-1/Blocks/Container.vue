8e18b4664458ae200bab96365f0f22e8e206b7d2
Alex <martmarchmartmarch@gmail.com>
2024-08-12 05:17:08 +0300

    update

---
+++ Vue/VueHW-1/Blocks/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h2>HW-1 Part2 Container</h2>
+    <movie-review></movie-review>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Container',
+
+  components: {
+    "movie-review": loadComponent("./Components/MovieReview.vue"),
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin-top: 20px;
+  background-color: rgb(236, 166, 166);
+  padding: 8px;
+}
+</style>
