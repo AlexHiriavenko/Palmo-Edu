ca067241e651be2145195371cd61769864baf2e2
Alex <martmarchmartmarch@gmail.com>
2024-08-18 19:30:49 +0300

    init: hw-4

---
+++ Vue/VueHW-4/readMe.md
@@ -0,0 +1,26 @@
+# VUE HW-4
+
+## Tasks:
+
+[tasks-vue-hw-4](https://gist.github.com/morphey312/a540b1e24fe49ef1f117ab6a426b4979)
+
+## Deploy:
+
+[deploy-link](https://alexhiriavenko.github.io/Palmo-Edu/VueHW-4/)
+
+## Branch
+
+https://github.com/AlexHiriavenko/Palmo-Edu/tree/vue-hw4
+
+## Source Code Path
+
+https://github.com/AlexHiriavenko/Palmo-Edu/tree/vue-hw4/Vue/VueHW-4/
+
+## Project setup:
+
+npm install; npm run serve; npm run build; npm run lint; npm run deploy
+
+## Notes:
+
+- чтобы запустить проект не меняя корневой дирректории (cd Vue/VueHW-4) <br>
+  можно ввести команду npm --prefix "./Vue/VueHW-4" install и npm --prefix "./Vue/VueHW-4" run serve
---
+++ Vue/VueHW-4/vue.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js', // Путь к основному скрипту страницы index
+      title: 'Vue HW-2', // Устанавливаем желаемый заголовок
+    },
+  },
+  // верный путь (basic url) для деплоя на gh-pages
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/Palmo-Edu/VueHW-4/' : '/',
+  configureWebpack: {
+    experiments: {
+      topLevelAwait: true,
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        // управляет включением или отключением Options API в Vue 3.
+        // false: Отключает Options API. Это уменьшает размер бандла и насильно заставляет использовать только Composition API.
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        // управляет включением или отключением Vue Devtools в продакшене.
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        // управляет включением или отключением подробных сообщений о несоответствии гидратации в продакшене.
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
+      }),
+    ],
+  },
+});
