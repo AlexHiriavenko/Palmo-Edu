656de838f1ffa6089ee36c6485d24f71c51f3029
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2024-09-27 17:06:04 +0300

    update: not important

--- PHP/HW-1/scalars.php
+++ PHP/HW-1/scalars.php
@@ -142,7 +142,25 @@ $var--;
 $var += 12;
 $var *= 7;
 $var -= 15;
-echo $var;
+echo $var . BR;
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../styles.css" rel="stylesheet">
+  <title>php scalars</title>
+
+</head>
+<body>
+  <section>
+    <ul>
+        <li><a href="../index.php">back to main</a></li>
+        <li><a href="./arrays.php">to arrays</a></li>
+    </ul>
+  </section>
+</body>
+</html>
