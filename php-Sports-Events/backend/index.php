b72ccb3f62b4bef1cd47fa940dc9d4c427d15323
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2024-10-15 17:33:23 +0300

    edit

--- php-Sports-Events/backend/index.php
+++ php-Sports-Events/backend/index.php
@@ -11,13 +11,13 @@
     <link rel="icon" href="./public/imgs/football.ico">
     <link rel="stylesheet" href="./public/styles/reset.css">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./public/styles/styles.css">
+    <link rel="stylesheet" href="/public/styles/styles.css">
     <title>Sports Events</title>
 </head>
 
 <body>
     <header class="app-header">
-        <?php include './views/navbar.php'; ?>
+        <?php include './views/app_header.php'; ?>
     </header>
     <main class="app-main"></main>
 </body>
