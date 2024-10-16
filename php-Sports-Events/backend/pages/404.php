11155a3d7f19feded0d216da2b045d985cd7ef39
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2024-10-16 13:40:47 +0300

    in progress

--- php-Sports-Events/backend/pages/404.php
+++ php-Sports-Events/backend/pages/404.php
@@ -16,7 +16,21 @@
 </head>
 
 <body>
-  <h1>Page Not Found</h1>
+  <main class="app-main pt-8 flex justify-center h-screen text-white">
+    <div class="container text-center">
+        <div class="mb-8">
+            <img src="/public/imgs/icon404.png" alt="404 icon" class="mx-auto w-80">
+        </div>
+        <h1 class="text-4xl font-bold mb-4">Whoops, 404</h1>
+        <p class="text-lg mb-6 text-2xl">Page not found </p>
+        <p class="text-lg mb-6 text-xl">
+            The page you were looking for does not exist or you do not have sufficient access rights.
+        </p>
+        <a href="/" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            Back to Home Page
+        </a>
+    </div>
+  </main> 
 </body>
 
 </html>
